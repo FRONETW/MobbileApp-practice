c235cfeeb86c38386065dbae5f4ea14d04d213b8
FRONETW <jkamedle@gmail.com>
2025-09-17 11:30:45 +0900

    Initial commit

---
+++ w03/src/main/java/com/example/w03/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.w03
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.w03.ui.theme.MyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyAppTheme {
+                HomeScreen()
+                }
+            }
+        }
+    }
+@Composable()
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+    {
+        Text("Compose Coffee",
+        style = MaterialTheme.typography.headlineMedium)
+
+        Image(
+            painter = painterResource(id = R.drawable.compose),
+            contentDescription = "Jetpack Compose 로고",
+            modifier = Modifier
+                .size(300.dp) // 이미지 크기 지정
+                .padding(16.dp)
+        )
+
+        Text("위치: 우송대 정문 앞",
+            style = MaterialTheme.typography.headlineMedium)
+        Button(onClick = {}) {  Text("Click pls")}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
